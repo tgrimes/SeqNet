abede0f1876148233c3dad4014c799caaaab7514
tgrimes <tyler.grimes@unf.edu>
2017-12-04 07:49:37 -0500

    add igraph

--- R/plot_network.R
+++ R/plot_network.R
@@ -21,7 +21,7 @@
 plot.network <- function(network, compare_graph = NULL,
                          weighted = NULL, as_subgraph = FALSE,
                          node_scale = 5, edge_scale = 1, 
-                         coords =  layout.fruchterman.reingold,
+                         coords =  igraph::layout.fruchterman.reingold,
                          main = "Untitled", ...) {
   if(class(network) == "network") {
     adj_matrix <- get_adj_matrix_from_network(network)
@@ -33,45 +33,45 @@ plot.network <- function(network, compare_graph = NULL,
     colnames(adj_matrix) <- 1:ncol(adj_matrix)
   }
   
-  g <- graph_from_adjacency_matrix(adj_matrix,
-                                   mode = "undirected",
-                                   weighted = weighted)
-  V(g)$size <- log(apply(adj_matrix, 2, sum) + 1) 
-  V(g)$size <- V(g)$size / max(V(g)$size) * node_scale
-  V(g)$frame.color <- "white"
+  g <- igraph::graph_from_adjacency_matrix(adj_matrix,
+                                           mode = "undirected",
+                                           weighted = weighted)
+  igraph::V(g)$size <- log(apply(adj_matrix, 2, sum) + 1) 
+  igraph::V(g)$size <- igraph::V(g)$size / max(igraph::V(g)$size) * node_scale
+  igraph::V(g)$frame.color <- "white"
   
   if(sum(adj_matrix) == 0 & !is.null(compare_graph)) {
     plot(compare_graph, vertex.color = "white", vertex.label.font = 2,
          vertex.label.color = "blue", vertex.label.cex = 0.7,
          edge.color = "white", main = main, ...)
   } else {
-    E(g)$width <- edge_scale
+    igraph::E(g)$width <- edge_scale
     if(as_subgraph) {
       if(!is.null(compare_graph)) {
-        g <- induced_subgraph(g, union(which(V(g)$size > 0), 
-                                       which(V(compare_graph)$size > 0)))
+        g <- igraph::induced_subgraph(g, union(which(igraph::V(g)$size > 0), 
+                                      which(igraph::V(compare_graph)$size > 0)))
       } else {
-        g <- induced_subgraph(g, which(V(g)$size > 0))
+        g <- igraph::induced_subgraph(g, which(igraph::V(g)$size > 0))
       }
     }
     
     edge.color <- "black"
     if(!is.null(compare_graph)) {
       h <- compare_graph %u% g
-      edge.color <- rep("red", length(E(h))) # Default color for edge in compared network is red.
-      subset_1 <- which(attr(E(h), "vnames") %in% attr(E(compare_graph), "vnames"))
+      edge.color <- rep("red", length(igraph::E(h))) # Default color for edge in compared network is red.
+      subset_1 <- which(attr(igraph::E(h), "vnames") %in% attr(igraph::E(compare_graph), "vnames"))
       edge.color[subset_1] <- 
-        ifelse(attr(E(h), "vnames")[subset_1] %in% attr(E(g), "vnames"), 
-               rep("black", length(E(compare_graph))),
-               rep("wheat", length(E(compare_graph))))
+        ifelse(attr(igraph::E(h), "vnames")[subset_1] %in% attr(igraph::E(g), "vnames"), 
+               rep("black", length(igraph::E(compare_graph))),
+               rep("wheat", length(igraph::E(compare_graph))))
       g <- h
       
-      V(g)$size <- log(apply(adj_matrix, 2, sum) + 1) 
-      V(g)$size <- V(g)$size / max(V(g)$size) * node_scale
-      V(g)$frame.color <- "white"
+      igraph::V(g)$size <- log(apply(adj_matrix, 2, sum) + 1) 
+      igraph::V(g)$size <- igraph::V(g)$size / max(igraph::V(g)$size) * node_scale
+      igraph::V(g)$frame.color <- "white"
       
       if(!is.null(compare_graph)) {
-        coords <- layout.fruchterman.reingold(compare_graph)
+        coords <- igraph::layout.fruchterman.reingold(compare_graph)
       }
     }
     
