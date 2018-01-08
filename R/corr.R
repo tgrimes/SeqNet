fe348231c7c2ce638904adc16a2441d6e946f0f4
tgrimes <tyler.grimes@unf.edu>
2018-01-08 10:38:13 -0500

    added spearman cor

--- R/corr.R
+++ R/corr.R
@@ -6,19 +6,27 @@
 #' @param x The n by p matrix of counts.
 #' @param threshold Cutoff for significant associations. If NULL, all correlations
 #' are returned. Otherwise, correlations at or below this threshold are set to zero.
+#' @param method The method used to compute correlations. Should be either "pearson"
+#'  or "spearman". The default value is set to spearman, which provides 
+#'  the more conservative estimation of associations.
 #' @return A list containing the p by p matrix of correlations,
-#' and the threshold used to determine significant associations. 
+#' the threshold used to determine significant associations, and
+#' the method used to compute the correlations.
 #' @export
-run_corr <- function(x, threshold = 0.9) {
+run_corr <- function(x, threshold = 0.9, method = "spearman") {
+  if(!(method %in% c("pearson", "spearman"))) {
+    stop('method should be one of c("pearson", "spearman").')
+  }
+  
   if(is.integer(x[1, 1])) {
     x <- x + 0.0
   }
-  scores <- cor(x)
+  scores <- cor(x, method = method)
   diag(scores) <- 0
   
   if(!is.null(threshold)) {
     scores[scores <= threshold] <- 0
   }
 
-  return(list(scores = scores, threshold = threshold))
+  return(list(scores = scores, threshold = threshold, method = method))
 }
