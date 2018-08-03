2287a6e6b43065f6ecc20356c576ef683601f476
tgrimes <tyler.grimes@unf.edu>
2018-08-03 18:04:56 -0400

    update

--- R/icorr.R
+++ R/icorr.R
@@ -1,31 +1,30 @@
-
-#' Wrapper for inverse correlation matrix
+#' Wrapper for inverse covariance matrix
 #' 
-#' Conducts co-expression analysis using partial correlations for association 
-#' measure, computed by inverting the correlation matrix.
-#' @param x The n by p matrix of counts.
+#' Conducts co-expression analysis using full partial correlations; these are
+#' computed by inverting the sample correlation matrix of the observations.
+#' @param x The n by p matrix of observations.
 #' @param threshold Cutoff for significant associations. If NULL, all correlations
 #' are returned. Otherwise, correlations of magnitude at or below this threshold are 
 #' set to zero.
-#' @param method The method used to compute correlations. Should be either "pearson"
-#'  or "spearman". The default is Spearman, which provides 
-#'  the more conservative estimation of associations.
-#' @return A list containing the p by p matrix of partial correlations,
-#' the threshold used to determine significant associations, and
-#' the method used to compute the correlations.
+#' @param k An integer that ensures the matrix inverse is numerically stable. 
+#' k = 1 is default; higher values will give less stable results.
+#' @return A list containing the p by p matrix of partial correlations and
+#' the threshold used to determine significant associations.
 #' @export
-run_icorr <- function(x, threshold = NULL, method = "pearson") {
-  if(!(method %in% c("pearson", "spearman"))) {
-    stop('method should be one of c("pearson", "spearman").')
-  }
-  
+run_icorr <- function(x, threshold = NULL, k = 1) {
   if(is.integer(x[1, 1])) {
     x <- x + 0.0
   }
-  scores <- cor(x, method = method)
+  scores <- cor(x)
   scores[is.na(scores) | is.nan(scores)] <- 0
-  scores <- scores + diag((1 + abs(min(eigen(scores)$values))), nrow(scores))
-  scores <- solve(scores)
+  
+  # Add constant to diagonal to ensure PD, then solve.
+  p <- nrow(scores)
+  eigen_vals <- eigen(scores)$values
+  scores <- scores + diag((max(eigen_vals) * 10^-k - min(eigen_vals)), p) * 
+    (min(eigen_vals) < max(eigen_vals) * 10^-k) # Add diag if this is TRUE.
+  
+  scores <- corpcor::cor2pcor(scores) # Computes inverse and handles negative signs.
   
   if(!is.null(threshold)) {
     scores[abs(scores) <= threshold] <- 0
@@ -33,5 +32,6 @@ run_icorr <- function(x, threshold = NULL, method = "pearson") {
   
   colnames(scores) <- colnames(x)
   
-  return(list(scores = scores, threshold = threshold, method = method))
+  return(list(scores = scores, 
+              threshold = threshold))
 }
--- R/pcor.R
+++ R/pcor.R
@@ -1,22 +1,22 @@
-
 #' Wrapper for partial correlations from corpcor
 #' 
-#' Conducts co-expression analysis using partial correlations for association 
-#' measure.
+#' Conducts co-expression analysis using full partial correlations; these are
+#' computed using the shrinkage approach for covariance estimation from the 
+#' corpcor package.
 #' @param x The n by p matrix of counts.
 #' @param threshold Cutoff for significant associations. If NULL, all correlations
 #' are returned. Otherwise, correlations of magnitude at or below this threshold are 
 #' set to zero.
-#' @return A list containing the p by p matrix of partial correlations,
-#' the threshold used to determine significant associations, and
-#' the method used to compute the correlations.
+#' @param verbose If true, progress messages will be printed from pcor.shrink().
+#' @return A list containing the p by p matrix of partial correlations and
+#' the threshold used to determine significant associations.
 #' @export
 run_pcor <- function(x, threshold = NULL, verbose = FALSE) {
   if(is.integer(x[1, 1])) {
     x <- x + 0.0
   }
   
-  scores <- pcor.shrink(x, verbose = verbose)
+  scores <- corpcor::pcor.shrink(x, verbose = verbose)
   
   if(!is.null(threshold)) {
     scores[abs(scores) <= threshold] <- 0
@@ -24,5 +24,6 @@ run_pcor <- function(x, threshold = NULL, verbose = FALSE) {
   
   colnames(scores) <- colnames(x)
   
-  return(list(scores = scores, threshold = threshold))
+  return(list(scores = scores, 
+              threshold = threshold))
 }
