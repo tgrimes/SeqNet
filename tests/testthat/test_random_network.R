a25f1fc2374a127c708d9d3bed41a9cc2136a7e5
tgrimes <tyler.grimes@unf.edu>
2019-05-03 13:47:49 -0400

    don't display plots

--- tests/testthat/test_random_network.R
+++ tests/testthat/test_random_network.R
@@ -2,6 +2,6 @@ testthat::test_that("networks structures can be created", {
   network <- random_network(100)
   expect_is(network, "network")
   expect_is(network$modules[[1]], "network_module")
-  expect_is(plot(network), "network_plot")
-  expect_is(plot_modules(network), "network_plot")
+  expect_is(plot(network, display_plot = FALSE), "network_plot")
+  expect_is(plot_modules(network, display_plot = FALSE), "network_plot")
 })
