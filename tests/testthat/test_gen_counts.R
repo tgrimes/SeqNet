20ecc42ecb867302533378f55d8dbb6f5c3f8890
tgrimes <tyler.grimes@unf.edu>
2019-05-28 13:45:16 -0400

    changed name gen_counts to gen_rnaseq

--- tests/testthat/test_gen_counts.R
+++ tests/testthat/test_gen_counts.R
@@ -1,11 +1,11 @@
 testthat::test_that("counts can be generated", {
   reference <- matrix(rpois(200, 10), nrow = 10, ncol = 20)
-  x <- gen_counts(10, gen_partial_correlations(create_empty_network(20)), reference)$x
+  x <- gen_rnaseq(10, gen_partial_correlations(create_empty_network(20)), reference)$x
   expect_is(x, "matrix")
-  x <- gen_counts(10, gen_partial_correlations(random_network(20)), reference)$x
+  x <- gen_rnaseq(10, gen_partial_correlations(random_network(20)), reference)$x
   expect_is(x, "matrix")
-  x <- gen_counts(20, gen_partial_correlations(random_network(20)), reference)$x
+  x <- gen_rnaseq(20, gen_partial_correlations(random_network(20)), reference)$x
   expect_is(x, "matrix")
-  x <- gen_counts(20, gen_partial_correlations(random_network(10)), reference)$x
+  x <- gen_rnaseq(20, gen_partial_correlations(random_network(10)), reference)$x
   expect_is(x, "matrix")
 })
