fd40808d75b8a7ec36097d6aa7b13a6d960c8604
tgrimes <tyler.grimes@unf.edu>
2019-06-18 18:07:30 -0400

    added imports

--- R/imports.R
+++ R/imports.R
@@ -4,8 +4,8 @@ NULL
 
 #' @importFrom grDevices adjustcolor colorRampPalette rgb
 #' @importFrom graphics legend plot
+#' @importFrom rlang .data
 #' @importFrom stats cov2cor dnbinom ecdf heatmap pnbinom pnorm qnbinom quantile 
 #' rbinom rnbinom rnorm runif sd var pbeta
 #' @importFrom utils data write.table
-#' @importFrom rlang .data
 NULL
